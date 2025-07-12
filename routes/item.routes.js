22d045b258455b7fcf53278fd3add064e474f66a
Aditya <adityarana206@gmail.com>
2025-07-12 16:11:25 +0400

    methodchange

--- routes/item.routes.js
+++ routes/item.routes.js
@@ -8,6 +8,6 @@ router.get('/getAllItems', getAllItems); // Get all items
 router.get('/getItemById/:id', getItemById); // Get item by ID
 router.put('/updateItem/:id', updateItem); // Update item by ID
 router.delete('/deleteItem/:id', deleteItem); // Delete item by ID    
-router.get('/categoryWiseItem',categoryWiseItem) // Category wise Item
+router.post('/categoryWiseItem',categoryWiseItem) // Category wise Item
 router.get('/subCategoryWiseItem',subCategoryWiseItem) // Sub category wise Item
 module.exports = router;
